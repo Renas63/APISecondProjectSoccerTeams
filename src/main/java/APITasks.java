08a6b0c54477a8e033521a35c41f950cfb3ab773
Renas63 <75856707+Renas63@users.noreply.github.com>
2021-04-14 12:50:09 -0500

    updated commit

--- src/main/java/APITasks.java
+++ src/main/java/APITasks.java
@@ -12,6 +12,7 @@ import org.junit.Assert;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -147,8 +148,29 @@ public class APITasks {
      * note: England team id is 66
      * Deserialization type: TypeReference
      */
-    public static List<String> getMidfielderFromBrazil() {
-        return null;
+    public static List<String> getMidfielderFromBrazil() throws URISyntaxException, IOException {
+        builder.setScheme("http").setHost("api.football-data.org").setPath("v2/teams/66");
+        HttpGet get = new HttpGet(builder.build());
+        get.addHeader("Accept", appJson);
+        get.addHeader("X-Auth-Token", token);
+        response = client.execute(get);
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains( appJson));
+        objectMapper = new ObjectMapper();
+        Map<String, Object> namesList = objectMapper.readValue(response.getEntity().getContent(),
+                new TypeReference<Map<String, Object>>() {
+                });
+        List<Map<String, Object>> squad =(List<Map<String, Object>>) namesList.get("squad");
+
+        List<String> midfielderFromBrazil= new ArrayList<>();
+        for (int i = 0; i <squad.size() ; i++) {
+            if(squad.get(i).get("countryOfBirth").equals("Brazil")&& squad.get(i).get("position").equals("Midfielder")){
+                midfielderFromBrazil.add(squad.get(i).get("name").toString());
+
+            }
+        }
+
+        return midfielderFromBrazil;
     }
 
     /*
@@ -156,8 +178,29 @@ public class APITasks {
      * note: England team id is 66
      * Deserialization type: TypeReference
      */
-    public static List<String> getAttackerFromEngland() {
-        return null;
+    public static List<String> getAttackerFromEngland() throws URISyntaxException, IOException {
+        builder.setScheme("http").setHost("api.football-data.org").setPath("v2/teams/66");
+        HttpGet get = new HttpGet(builder.build());
+        get.addHeader("Accept", appJson);
+        get.addHeader("X-Auth-Token", token);
+        response = client.execute(get);
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains( appJson));
+        objectMapper = new ObjectMapper();
+        Map<String, Object> namesList = objectMapper.readValue(response.getEntity().getContent(),
+                new TypeReference<Map<String, Object>>() {
+                });
+        List<Map<String, Object>> squad =(List<Map<String, Object>>) namesList.get("squad");
+
+        List<String> listOfAttackerFromEngland= new ArrayList<>();
+        for (int i = 0; i <squad.size() ; i++) {
+            if(squad.get(i).get("position").equals("Attacker")&& squad.get(i).get("countryOfBirth").equals("England")){
+                listOfAttackerFromEngland.add(squad.get(i).get("name").toString());
+            }
+        }
+
+
+        return listOfAttackerFromEngland;
     }
 
     /*
@@ -165,16 +208,60 @@ public class APITasks {
      * note: Spain team id is 77
      * Deserialization type: TypeReference
      */
-    public static List<String> getSpainCoach() {
-        return null;
+    public static List<String> getSpainCoach() throws URISyntaxException, IOException {
+        builder.setScheme("http").setHost("api.football-data.org").setPath("v2/teams/77");
+        HttpGet get = new HttpGet(builder.build());
+        get.addHeader("Accept", appJson);
+        get.addHeader("X-Auth-Token", token);
+        response = client.execute(get);
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains( appJson));
+        objectMapper = new ObjectMapper();
+        Map<String, Object> namesList = objectMapper.readValue(response.getEntity().getContent(),
+                new TypeReference<Map<String, Object>>() {
+                });
+        List<Map<String, Object>> squad= (List<Map<String, Object>>)namesList.get("squad");
+
+        List<String> coachName= new ArrayList<>();
+        for (int i = 0; i < squad.size(); i++) {
+            if(squad.get(i).get("role").equals("COACH")){
+                coachName.add(squad.get(i).get("name").toString());
+                System.out.println(squad.get(i).get("name"));
+            }
+        }
+
+
+        return coachName;
     }
 
     /*
      * GET list of all competitions
      * Deserialization type: TypeReference
      */
-    public static List<String> getAllCompetitions() {
-        return null;
+    public static List<String> getAllCompetitions() throws URISyntaxException, IOException {
+        builder.setScheme("http").setHost("api.football-data.org").setPath("v2/competitions/");
+        HttpGet get = new HttpGet(builder.build());
+        get.addHeader("Accept", appJson);
+        get.addHeader("X-Auth-Token", token);
+        response = client.execute(get);
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains( appJson));
+        objectMapper = new ObjectMapper();
+        Map<String, Object> namesList = objectMapper.readValue(response.getEntity().getContent(),
+                new TypeReference<Map<String, Object>>() {
+                });
+
+        List< Map<String, Object>> competitionsList= (List<Map<String, Object>>)namesList.get("competitions");
+
+        List<String> allCompetitions= new ArrayList<>();
+        for (int i = 0; i <competitionsList.size() ; i++) {
+          allCompetitions.add(competitionsList.get(i).get("name").toString());
+
+            System.out.println(competitionsList.get(i).get("name"));
+        }
+
+
+        return allCompetitions;
 
     }
 
@@ -184,7 +271,42 @@ public class APITasks {
      * note: endpoint for scorers: `competitions/<year>/scorers`
      * Deserialization type: TypeReference
      */
-    public static List<String> getSecondHighestScorer() {
-        return null;
+    public static List<String> getSecondHighestScorer() throws URISyntaxException, IOException {
+        builder.setScheme("http").setHost("api.football-data.org").setPath("v2/competitions/2000/scorers");
+        HttpGet get = new HttpGet(builder.build());
+        get.addHeader("Accept", appJson);
+        get.addHeader("X-Auth-Token", token);
+        response = client.execute(get);
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains( appJson));
+        objectMapper = new ObjectMapper();
+        Map<String, Object> parsedResponse = objectMapper.readValue(response.getEntity().getContent(),
+                new TypeReference<Map<String, Object>>() {
+                });
+        List<Map<String ,Object>> teams= ( List<Map<String ,Object>>)parsedResponse.get("scorers");
+        Object [] arr=new Object[teams.size()];
+        for (int i = 0; i < teams.size(); i++) {
+            arr[i]=teams.get(i).get("numberOfGoals");
+//            System.out.println(String.valueOf(teams.get(i).get("numberOfGoals")));
+        }
+        Arrays.sort(arr);
+        Object secondHighestGoal=arr[arr.length-1];
+        for(int i= arr.length-1;i>=0;i--){
+            if(arr[i]!=secondHighestGoal){
+                secondHighestGoal=arr[i];
+                break;
+            }
+        }
+        List<String> secondHighestScorer=new ArrayList<>();
+        for(int i=0;i< teams.size();i++){
+            if(teams.get(i).get("numberOfGoals").equals(secondHighestGoal)){
+                Map<String,Object> names=(Map<String,Object>)(teams.get(i).get("player"));
+                secondHighestScorer.add((String)names.get("name"));
+                System.out.println(names.get("name"));
+            }
+        }
+        return secondHighestScorer;
     }
-}
+
+    }
+
